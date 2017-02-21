00e1c608d4950c4ae15b2ce72e0cc735a049b224
yanbin.liu <yanbin.liu@oakton.com.au>
2017-02-21 19:52:03 +1100

    Fix the edit all fields button error when there is no field in the component

--- src/Foundation/FieldEditor/code/Services/GetFieldsToEditService.cs
+++ src/Foundation/FieldEditor/code/Services/GetFieldsToEditService.cs
@@ -5,29 +5,33 @@ using System.Web;
 
 namespace Sitecore.Foundation.FieldEditor.Services
 {
-  using System.Collections.Specialized;
-  using Sitecore.Data;
-  using Sitecore.Data.Items;
-  using Sitecore.Shell.Applications.WebEdit;
+    using System.Collections.Specialized;
+    using Sitecore.Data;
+    using Sitecore.Data.Items;
+    using Sitecore.Shell.Applications.WebEdit;
 
-  public class GetFieldsToEditService
-  {
-    public static string GetFieldsToEdit(Item item)
+    public class GetFieldsToEditService
     {
-      var customFields = item.Template.Fields.Where(x => !x.Name.StartsWith("__"));
-      var pipedFieldNames = String.Join("|", customFields.Select(f => f.Name));
-      return pipedFieldNames;
-    }
+        public static string GetFieldsToEdit(Item item)
+        {
+            var customFields = item.Template.Fields.Where(x => !x.Name.StartsWith("__"));
+            if (!customFields.Any())
+            {
+                return String.Empty;
+            }
+            var pipedFieldNames = String.Join("|", customFields.Select(f => f.Name));
+            return pipedFieldNames;
+        }
 
-    public static PageEditFieldEditorOptions GetFieldEditorOptions(NameValueCollection form, string pipedFields, Item item)
-    {
-      var fields = pipedFields.Split('|').Where(fieldName => item.Fields[fieldName] != null).Select(fieldName => new FieldDescriptor(item, fieldName)).ToList();
-      var options = new PageEditFieldEditorOptions(form, fields)
-                    {
-                      PreserveSections = true,
-                      ShowSections = true
-                    };
-      return options;
+        public static PageEditFieldEditorOptions GetFieldEditorOptions(NameValueCollection form, string pipedFields, Item item)
+        {
+            var fields = pipedFields.Split('|').Where(fieldName => item.Fields[fieldName] != null).Select(fieldName => new FieldDescriptor(item, fieldName)).ToList();
+            var options = new PageEditFieldEditorOptions(form, fields)
+            {
+                PreserveSections = true,
+                ShowSections = true
+            };
+            return options;
+        }
     }
-  }
 }
